00cb9d20a32c32888eac0a52cd22679d8ecb0eab
Tóth <tothbalint719@gmail.com>
2020-01-29 15:15:36 +0100

    lutz update

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import idle from 'lutzjs'
 export default class IdlePlugin {
   static install(Vue, options) {
 
-    Vue.prototype.$onIdle = function (func) {
-      idle.on(func)
+    Vue.prototype.$onIdle = function (timeString, func) {
+      idle.on(timeString, func)
     }
 
-    Vue.prototype.$watchIdle = function (millis, _window) {
-      const _millis = millis || options.millis || undefined
-      idle.watch(_millis, _window)
+    Vue.prototype.$observeIdle = function () {
+      idle.watch()
     }
 
     Vue.prototype.$ignoreIdle = function () {
